239757d0ccbe0d6412b3d000c9eed07976bee8e3
DESKTOP-MF8U07A\Jelena <jelena.popadic.1999@gmail.com>
2020-11-24 20:11:09 +0100

    Web aplikacija

---
+++ PresentationLayer/PresentationWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="PresentationWeb.Global_asax" Language="vb" %>
---
+++ PresentationLayer/WebPresentation/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using BussinessLayer;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebPresentation
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            StudentBussiness sb = new StudentBussiness();
+            List<Student> studentList = sb.GetStudents();
+            foreach (Student s in studentList)
+                ListBox1.Items.Add(s.ToString());
+        }
+    }
+}
